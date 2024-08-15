6aad839b032ac2eb8eeee6187a29b7f206a0131b
Hermayonick-Catalina-Hernandez-Gonzalez <2130036@upv.edu.mx>
2024-08-15 08:12:38 -0600

    Terminado

--- resources/views/admin/registro-enfermeros.blade.php
+++ resources/views/admin/registro-enfermeros.blade.php
@@ -24,7 +24,7 @@
     <div class="py-12 flex justify-center">
         <div class="max-w-lg w-full sm:px-6 lg:px-8">
             <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <form method="post" action="{{ route('admin.registro-enfermeros.store') }}">
+                <form method="post" action="{{ route('admin.registro-enfermeros') }}">
                     @csrf
                     <!-- Nombre -->
                     <div>
